485dbb51afeeaa74735595422c8db324aa05d402
Adhika Mahendra <amahendra@ubuntu.(none)>
2012-08-02 16:54:21 -0400

    creating layover window popup

--- static/js/home.js
+++ static/js/home.js
@@ -1,21 +1,44 @@
 $(document).ready(function(){
-  $('.item_icon > a').click(function(){
-    // Show description
-    var hash_name = $(this).attr('rel');
-    $('.item_description').slideUp();
-    $('#twitter').fadeOut();
-    $(hash_name).delay(400).slideDown();
+	
+	$('.item_icon > a').click(function(){
+	  
+		// Show description
+		var hash_name = $(this).attr('rel');
+		$('.item_description').slideUp();
+		$('#twitter').fadeOut();
+		$(hash_name).delay(400).slideDown();
+		
+		// Animate clicked icon
+		$().animate();
+		$('.item_icon > a').removeClass('active');
+		$(this).addClass('active');
     
-    // Animate clicked icon
-    $().animate();
-    $('.item_icon > a').removeClass('active');
-    $(this).addClass('active');
-  });
+	});
   
-  $('.close').click(function() {
-    // Close description
-    $('.item_description').slideUp();
-    $('.item_icon > a').removeClass('active');
-    $('#twitter').delay(400).fadeIn();
-  });
+	$('.close').click(function() {
+	  
+		// Close description
+		$('.item_description').slideUp();
+		$('.item_icon > a').removeClass('active');
+		$('#twitter').delay(400).fadeIn();
+    
+	});
+	
+	$('body').click(function() {
+	
+		// Create overlay elements
+		$('body').prepend('<div id="overlay_background"></div>');
+		$('body').prepend('<div id="overlay_window"></div>');
+		$('#overlay_window').prepend('<form name="email_me" id="email_me" method="post" action="#sent"></form>');
+		$('#email_me').prepend('<div><input type="submit" value="Send Email" /></div>');
+		$('#email_me').prepend('<div><textarea name="message" id="message">Comment</textarea></div>');
+		$('#email_me').prepend('<div><label>From: </label><input type="text" name="from" id="from" /></div>');
+
+		// Expand overlay elements
+		$('#overlay_background').height($(window).height());
+		$('#overlay_background').width($(window).width());
+		$('#overlay_window').css('top', ($(window).height() / 2) - ($('#overlay_window').height() / 2));
+		$('#overlay_window').css('left', ($(window).width() / 2) - ($('#overlay_window').width() / 2));
+	
+	});
 });
--- system/libraries/Email.php
+++ system/libraries/Email.php
@@ -30,22 +30,22 @@ class CI_Email {
 
 	var	$useragent		= "CodeIgniter";
 	var	$mailpath		= "/usr/sbin/sendmail";	// Sendmail path
-	var	$protocol		= "mail";	// mail/sendmail/smtp
-	var	$smtp_host		= "";		// SMTP Server.  Example: mail.earthlink.net
-	var	$smtp_user		= "";		// SMTP Username
-	var	$smtp_pass		= "";		// SMTP Password
-	var	$smtp_port		= "25";		// SMTP Port
-	var	$smtp_timeout	= 5;		// SMTP Timeout in seconds
-	var	$smtp_crypto	= "";		// SMTP Encryption. Can be null, tls or ssl.
+	var	$protocol		= "smtp";	// mail/sendmail/smtp
+	var	$smtp_host		= "smtp.googlemail.com";		// SMTP Server.  Example: mail.earthlink.net
+	var	$smtp_user		= "admin@dhimoet.com";		// SMTP Username
+	var	$smtp_pass		= "blah";		// SMTP Password
+	var	$smtp_port		= "465";		// SMTP Port
+	var	$smtp_timeout	= 7;		// SMTP Timeout in seconds
+	var	$smtp_crypto	= "ssl";		// SMTP Encryption. Can be null, tls or ssl.
 	var	$wordwrap		= TRUE;		// TRUE/FALSE  Turns word-wrap on/off
 	var	$wrapchars		= "76";		// Number of characters to wrap at.
-	var	$mailtype		= "text";	// text/html  Defines email formatting
+	var	$mailtype		= "html";	// text/html  Defines email formatting
 	var	$charset		= "utf-8";	// Default char set: iso-8859-1 or us-ascii
 	var	$multipart		= "mixed";	// "mixed" (in the body) or "related" (separate)
 	var $alt_message	= '';		// Alternative message for HTML emails
-	var	$validate		= FALSE;	// TRUE/FALSE.  Enables email validation
+	var	$validate		= TRUE;	// TRUE/FALSE.  Enables email validation
 	var	$priority		= "3";		// Default priority (1 - 5)
-	var	$newline		= "\n";		// Default newline. "\r\n" or "\n" (Use "\r\n" to comply with RFC 822)
+	var	$newline		= "\r\n";		// Default newline. "\r\n" or "\n" (Use "\r\n" to comply with RFC 822)
 	var $crlf			= "\n";		// The RFC 2045 compliant CRLF for quoted-printable is "\r\n".  Apparently some servers,
 									// even on the receiving end think they need to muck with CRLFs, so using "\n", while
 									// distasteful, is the only thing that seems to work for all environments.
