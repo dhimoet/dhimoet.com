dfeaa932b697fd585600d9e58b69fea2c21f972e
Adhika Mahendra <amahendra@ubuntu.(none)>
2012-09-13 10:07:18 -0400

    bhs

---
+++ bhs/application/views/templates/base_header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<link rel="SHORTCUT ICON" href="http://bhs-consultants.com/wp-content/themes/newBHS/images/favicon.png" />
+	<link rel="stylesheet" type="text/css" href="/public/css/style.css" />
+	<script type="text/javascript" src="/public/js/jquery.js"></script>
+	
+	<title>BHS Consultants | Welcome To Our New Website!</title>
+</head>
+<body>
+	
+<div id="header">
+	<div class="logo"><img src="/public/img/mobileLogo.png" /></div>
+	<div class="slider"><div class="button">&nbsp;</div></div>
+</div>
