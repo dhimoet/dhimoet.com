131a9147fc6166841cae455d25a5f22bbe8e4ef6
Adhika Mahendra <amahendra@ubuntu.(none)>
2012-09-13 11:05:35 -0400

    bhs

--- bhs/application/config/config.php
+++ bhs/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://www.dhimoet.com/bhs';
+$config['base_url']	= 'http://www.dhimoet.dev/bhs';
 
 /*
 |--------------------------------------------------------------------------
--- bhs/application/views/home/index.php
+++ bhs/application/views/home/index.php
@@ -5,7 +5,7 @@
 		</div>
 		<p class="font_small font_grey">BHS Consultants is your “one-stop shop” for all of your technology needs.</p>
 		<p class="font_small font_grey">Having been founded in 1994, BHS Consultants has implemented technology-based solutions in countless industries. A partial list of our expertise includes the following:</p>
-		<p><img src="<?=base_url()?>/public/img/homeColumnImg01.jpg" /></p>
+		<p><img src="<?=base_url()?>public/img/homeColumnImg01.jpg" /></p>
 		<ul class="font_small font_grey">
 			<li>Custom web solutions</li>
 			<li>Cutting-edge web design</li>
@@ -14,7 +14,7 @@
 			<li>Creative and artistic design</li>
 			<li>IP phone integration</li>
 		</ul>
-		<p><img src="<?=base_url()?>/public/img/homeColumnImg02.jpg" /></p>
+		<p><img src="<?=base_url()?>public/img/homeColumnImg02.jpg" /></p>
 		<ul class="font_small font_grey">
 			<li>Project management services</li>
 			<li>Search Engine Optimization (SEO)</li>
@@ -22,7 +22,7 @@
 			<li>On-site and off-shore Digital Media back end work such as offshore editing, and transcoding management</li>
 			<li>Hardware solutions</li>
 		</ul>
-		<p><img src="<?=base_url()?>/public/img/homeColumnImg03.jpg" /></p>
+		<p><img src="<?=base_url()?>public/img/homeColumnImg03.jpg" /></p>
 		<ul class="font_small font_grey">
 			<li>Off-the-shelf software solutions customizable for your business</li>
 			<li>Consultation to identify ways to help your business grow</li>
@@ -31,7 +31,7 @@
 			<li>Communication solutions for all companies</li>
 			<li>24/7 Support for all your IT needs</li>
 		</ul>
-		<p class="font_small font_grey">If you’re curious as to what we can do for you and/or you don’t see what you’re looking for in this list, please <a href="<?=base_url()?>/contact/">contact</a> us as we’d love to discuss your needs!</p>
+		<p class="font_small font_grey">If you’re curious as to what we can do for you and/or you don’t see what you’re looking for in this list, please <a href="<?=base_url()?>contact/">contact</a> us as we’d love to discuss your needs!</p>
 		<hr />
 	</div>
 	
@@ -39,8 +39,8 @@
 		<div class="font_medium font_blue">
 			Clients
 		</div>
-		<p><a href="<?=base_url()?>/home/clients"><img src="<?=base_url()?>/public/img/clientsImgLink.png" /></a></p>
-		<p class="font_small font_grey"><a href="<?=base_url()?>/home/clients">Click here</a> for a full list of our clients..</p>
+		<p><a href="<?=base_url()?>home/clients"><img src="<?=base_url()?>public/img/clientsImgLink.png" /></a></p>
+		<p class="font_small font_grey"><a href="<?=base_url()?>home/clients">Click here</a> for a full list of our clients..</p>
 		<hr />
 	</div>
 	
@@ -48,7 +48,7 @@
 		<div class="font_medium font_blue">
 			Case Studies
 		</div>
-		<p class="font_small font_grey"><a href="<?=base_url()?>/home/case-studies">Browse</a> through case studies on what BHS Consultants has done.</p>
+		<p class="font_small font_grey"><a href="<?=base_url()?>home/case-studies">Browse</a> through case studies on what BHS Consultants has done.</p>
 		<hr />
 	</div>
 	
@@ -56,7 +56,7 @@
 		<div class="font_medium font_blue">
 			Testimonials
 		</div>
-		<p class="font_small font_grey"><a href="<?=base_url()?>/home/testimonials">Check out</a> what some of our clients have to say about us!</p>
+		<p class="font_small font_grey"><a href="<?=base_url()?>home/testimonials">Check out</a> what some of our clients have to say about us!</p>
 		<hr />
 	</div>
 	
@@ -64,7 +64,7 @@
 		<div class="font_medium font_blue">
 			Apply Now
 		</div>
-		<p class="font_small font_grey"><a href="<?=base_url()?>/home/apply-now">IT Services Sales Specialist</a></p>
+		<p class="font_small font_grey"><a href="<?=base_url()?>home/apply-now">IT Services Sales Specialist</a></p>
 		<hr />
 	</div>
 	
@@ -73,7 +73,7 @@
 			Digital Media Processing
 		</div>
 		<p class="font_small font_grey">Have an old file that you cannot open using your existing software, or a video format that needs transcoding to play on your hardware?</p>
-		<p class="font_small font_grey"><a href="<?=base_url()?>/home/contact">Contact us</a> with the details and we will see what we can do for you.</p>
+		<p class="font_small font_grey"><a href="<?=base_url()?>home/contact">Contact us</a> with the details and we will see what we can do for you.</p>
 		<hr />
 	</div>
 </div>
